023eba234b8b47785e7add569dfe06506337adb2
Henrysoed <henry.soedibjo@gmail.com>
2024-05-25 21:27:08 +0700

    Implementation and Connect FE Login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useState } from 'react';
 import Link from "next/link";
 import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
@@ -5,6 +8,26 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        const response = await fetch('https://auth-p5zxnxph7q-ew.a.run.app/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        });
+
+        if (response.ok) {
+            const { token } = await response.json();
+            localStorage.setItem('token', token);
+            console.log('Logged in token: ' + localStorage.getItem('token'));
+        }
+    };
+
     return (
         <div className="flex items-center justify-center h-screen bg-gray-200">
             <Card className="mx-auto max-w-lg w-1/3">
@@ -14,14 +37,14 @@ export default function LoginPage() {
                     </CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <div className="space-y-4">
+                    <form onSubmit={handleLogin} className="space-y-4">
                         <div className="space-y-2">
                             <Label htmlFor="email">Email</Label>
-                            <Input id="email" required type="email" />
+                            <Input id="email" required type="email" value={email} onChange={e => setEmail(e.target.value)} />
                         </div>
                         <div className="space-y-2">
                             <Label htmlFor="password">Password</Label>
-                            <Input id="password" required type="password" />
+                            <Input id="password" required type="password" value={password} onChange={e => setPassword(e.target.value)} />
                         </div>
                         <Button className="w-full" type="submit">
                             Login
@@ -37,7 +60,7 @@ export default function LoginPage() {
                                 </Link>
                             </p>
                         </div>
-                    </div>
+                    </form>
                 </CardContent>
             </Card>
         </div>
