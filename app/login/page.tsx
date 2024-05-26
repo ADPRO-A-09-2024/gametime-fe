7d066842e3fceb1eb942c808288c7f8f289feb27
RobertBenyamin <robertbenyamino@gmail.com>
2024-05-26 15:57:50 +0700

    feat: update login page to handle errors and redirect if success

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,47 +1,64 @@
 'use client'
 
-import { useState } from 'react';
 import Link from "next/link";
-import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
+import { useState } from 'react';
+import { useRouter } from "next/navigation";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const router = useRouter();
 
     const handleLogin = async (e: { preventDefault: () => void; }) => {
         e.preventDefault();
-        const response = await fetch('https://auth-p5zxnxph7q-ew.a.run.app/auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        });
+        setIsLoading(true);
+        setErrorMessage(null);
 
-        if (response.ok) {
-            const { token } = await response.json();
-            localStorage.setItem('token', token);
-            console.log('Logged in token: ' + localStorage.getItem('token'));
-        }else{
-            console.log('Login failed');
-        }
+        try {
+            const response = await fetch('https://auth-p5zxnxph7q-ew.a.run.app/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
 
-        const responseUser = await fetch('https://auth-p5zxnxph7q-ew.a.run.app/user/me', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem('token')}`,
-            },
-        });
+            if (response.ok) {
+                const { token } = await response.json();
+                localStorage.setItem('token', token);
+                console.log('Logged in token: ' + localStorage.getItem('token'));
+            } else {
+                const {description} = await response.json();
+                throw new Error(description);
+            }
 
-        if (responseUser.ok) {
-            const { id, email } = await responseUser.json();
-            localStorage.setItem('idUser', id);
-            localStorage.setItem('email', email);
-            console.log('Logged in idUser: ' + localStorage.getItem('idUser'));
-            console.log('Logged in email: ' + localStorage.getItem('email'));
+            const responseUser = await fetch('https://auth-p5zxnxph7q-ew.a.run.app/user/me', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                },
+            });
+    
+            if (responseUser.ok) {
+                const { id, email } = await responseUser.json();
+                localStorage.setItem('idUser', id);
+                localStorage.setItem('email', email);
+                console.log('Logged in idUser: ' + localStorage.getItem('idUser'));
+                console.log('Logged in email: ' + localStorage.getItem('email'));
+                
+                router.push("/"); // Redirect to home page
+            }
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message : 'Login failed!';
+            setErrorMessage(errorMessage);
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -63,8 +80,11 @@ export default function LoginPage() {
                             <Label htmlFor="password">Password</Label>
                             <Input id="password" required type="password" value={password} onChange={e => setPassword(e.target.value)} />
                         </div>
-                        <Button className="w-full" type="submit">
-                            Login
+                        {errorMessage && (
+                            <p className="text-red-500 font-semibold text-center">{errorMessage}</p>
+                        )}
+                        <Button className="w-full" type="submit" disabled={isLoading}>
+                            {isLoading ? 'Loading...' : 'Login'}
                         </Button>
                         <div className="text-center">
                             <p className="text-grey-dark text-sm">
