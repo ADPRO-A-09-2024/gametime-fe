39bef06b01c8f2b0e7b753739a1dbde9e5aecaa5
Nadya Hoesin <nadya.hoesin@ui.ac.id>
2024-05-26 18:43:05 +0700

    Create page for creating product and seeing product details

--- app/product/create/page.tsx
+++ app/product/create/page.tsx
@@ -1,105 +1,69 @@
 'use client'
 
-import React, { useState } from 'react';
-import Link from "next/link";
+import { useState } from 'react';
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
+import api from './api';
 
-const CreateProductPage: React.FC = () => {
-    const [name, setName] = useState(''); 
-    const [description, setDescription] = useState(''); 
-    const [category, setCategory] = useState(''); 
-    const [price, setPrice] = useState(''); 
+const CreateProductPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
 
-    const createProduct = (event: React.FormEvent) => {
-        event.preventDefault();
+  const handleSubmitCreate = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
 
-        // Construct the URLs
-        const urlCreate = "http://localhost:8080/product/create/";
+    const form = e.currentTarget;
+    const formData = new FormData(form);
 
-        // Fetch the data
-        const dataCreate = fetch(urlCreate, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                "sellerId": `${}`,
-                "name": `${name}`,
-                "description": `${description}`,
-                "category": `${category}`,
-                "price": `${price}`
-            })
-        }).then(response => response.json());
+    const name = formData.get('name') as string;
+    const description = formData.get('description') as string;
+    const category = formData.get('category') as string;
+    const price = formData.get('price') as string;
 
-        // Do something with the data
-        console.log(dataCreate);
-    };
-    return (
-        <div className="flex items-center justify-center h-screen bg-gray-200">
-            <div className="max-w-lg w-full p-8 bg-white rounded-lg shadow-lg">
-                <h2 className="text-2xl font-bold text-center mb-4">Create Product</h2>
-                <form onSubmit={createProduct}>
-                    <div className="mb-4">
+    await api.post('/product/create', {
+        sellerId: localStorage.getItem('idUser'),
+        name, description, category, price 
+        }).then(response => {
+            window.location.href = `/product/get/${response.data.id}`;
+        })
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+        <Card className="mx-auto max-w-lg w-1/3">
+            <CardHeader className="space-y-1">
+                <CardTitle className="text-2xl font-bold text-center">
+                    Create Product
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <form onSubmit={handleSubmitCreate} className="space-y-4">
+                    <div className="space-y-2">
                         <Label htmlFor="name">Name</Label>
-                        <Input
-                            id="name"
-                            required
-                            type="text"
-                            placeholder="Enter product name"
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
+                        <Input id="name" required type="text" name='name' />
                     </div>
-                    <div className="mb-4">
+                    <div className="space-y-2">
                         <Label htmlFor="description">Description</Label>
-                        <Input
-                            id="description"
-                            required
-                            type="text"
-                            placeholder="Enter product description"
-                            value={description}
-                            onChange={e => setDescription(e.target.value)}
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
+                        <Input id="description" required type="text" name='description' />
                     </div>
-                    <div className="mb-4">
+                    <div className="space-y-2">
                         <Label htmlFor="category">Category</Label>
-                        <Input
-                            id="category"
-                            required
-                            type="text"
-                            placeholder="Enter product category"
-                            value={category}
-                            onChange={e => setCategory(e.target.value)}
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
+                        <Input id="category" required type="text" name='category' />
                     </div>
-                    <div className="mb-8">
+                    <div className="space-y-2">
                         <Label htmlFor="price">Price</Label>
-                        <Input
-                            id="ratingMoreThan"
-                            type="number"
-                            placeholder="Enter product price"
-                            value={price}
-                            onChange={e => setPrice(e.target.value)}
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
+                        <Input id="price" required type="number" name='price' />
                     </div>
-                    <div className="flex items-center justify-center">
-                        <Button
-                            type="submit"
-                            className="w-full px-4 py-2 bg-indigo-600 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Create
-                        </Button>
-                    </div>
+                    <Button className="w-full" type="submit" disabled={isLoading}>
+                        {isLoading ? 'Loading...' : 'Create'}
+                    </Button>
                 </form>
-            </div>
-        </div>
-    );
+            </CardContent>
+        </Card>
+    </div>
+  );
 };
 
 export default CreateProductPage;
--- app/product/get/[id]/page.tsx
+++ app/product/get/[id]/page.tsx
@@ -1,9 +1,99 @@
-const Page = ({ params }: { params: { id: string } }) => {
-    return (
-        <div className="min-h-screen flex justify-center items-center gap-2 flex-wrap">
-            <h1>Hello, {params.id}!</h1>
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import api from './api';
+
+interface Product {
+    id: string;
+    name: string;
+    description: string;
+    category: string;
+    price: number;
+}
+
+const ProductPage = () => {
+  const pathname = usePathname();
+  const id = pathname.split('/')[3];
+
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    api.get(`product/get/${id}`)
+      .then((response: { data: { id: any; name: any; description: any; category: any; price: any; }; }) => {
+        setProduct({
+            id: response.data.id,
+            name: response.data.name,
+            description: response.data.description,
+            category: response.data.category,
+            price: response.data.price
+        })
+      })
+  });
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
+  const reviewUrl = `/product/${product.id}/review`
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+    <div className="max-w-lg w-full p-8 bg-white rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-center mb-4">Product</h2>
+        <div className="space-y-4">
+            <div className="mb-4">
+                <Label htmlFor="name">name</Label>
+                <Input
+                    id="name"
+                    value={product.name}
+                    readOnly
+                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+            <div className="mb-4">
+                <Label htmlFor="description">Description</Label>
+                <Input
+                    id="description"
+                    value={product.description}
+                    readOnly
+                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+            <div className="mb-4">
+                <Label htmlFor="category">Category</Label>
+                <Input
+                    id="category"
+                    value={product.category}
+                    readOnly
+                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+            <div className="mb-8">
+                <Label htmlFor="price">Price</Label>
+                <Input
+                    id="price"
+                    value={product.price}
+                    readOnly
+                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
         </div>
-    );
+        <div className="mt-4 text-center">
+            <p className="text-sm text-gray-600">
+                See review{' '}
+                <Link href={reviewUrl}>
+                    <span className="font-medium text-indigo-600 hover:text-indigo-500">Review</span>
+                </Link>
+            </p>
+        </div>
+    </div>
+</div>
+  );
 };
 
-export default Page;
+export default ProductPage;
+
